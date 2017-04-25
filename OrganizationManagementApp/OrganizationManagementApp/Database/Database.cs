d16cb9c17e6e819af9a270605925463f0f2865b8
syed tauhid ahmed <stahmed001@gmail.com>
2017-04-25 19:34:00 +0600

    init project

---
+++ OrganizationManagementApp/OrganizationManagementApp/Database/Database.cs
@@ -0,0 +1,86 @@
+﻿using Starcounter;
+
+[Database]
+public class Organization
+{
+    public QueryResultRows<Company> CompanyList = Db.SQL<Company>("SELECT c From Company c");
+}
+
+[Database]
+public class Company
+{
+    public string Name;
+    public QueryResultRows<Person> PersonList => Db.SQL<Person>("SELECT p FROM Person p WHERE p.Company=?", this);
+}
+
+[Database]
+public class Person
+{
+    public Company Company;
+    public string Street;
+    public string City;
+    public string Country;
+    public string Email;
+    public int TotalSale => Db.SQL<int>("SELECT COUNT(s.Saler) FROM Sales s WHERE s.Saler=?", this).First;
+    public decimal TotalCommission => Db.SQL<decimal>("SELECT SUM(s.Commission) FROM Sales s WHERE s.Saler=?", this).First;
+    public QueryResultRows<Sales> Sales => Db.SQL<Sales>("SELECT s FROM Sales s WHERE s.Saler=?", this);
+
+    public decimal AvgCommission
+    {
+        get
+        {
+            return TotalCommission / TotalSale;
+        }
+    }
+}
+
+[Database]
+public class Sales
+{
+    public Person Saler;
+    public string Street;
+    public string City;
+    public string Country;
+    public decimal Price;
+    public decimal Commission;
+    public string Date;
+}
+
+//[Database]
+//public class Invoice {
+//    public int InvoiceNo;
+//    public string Name;
+//    public decimal Total {
+//        get {
+//            return Db.SQL<decimal>("SELECT sum(r.Total) FROM InvoiceRow r WHERE r.Invoice=?", this).First;
+//        }
+//    }
+//    public QueryResultRows<InvoiceRow> Items {
+//        get {
+//            return Db.SQL<InvoiceRow>("SELECT r FROM InvoiceRow r WHERE r.Invoice=?", this);
+//        }
+//    }
+
+//    public Invoice() {
+//        new InvoiceRow() {
+//          Invoice = this
+//        };
+//    }
+//}
+
+//[Database]
+//public class InvoiceRow {
+//    public Invoice Invoice;
+//    public string Description;
+//    public int Quantity;
+//    public decimal Price;
+//    public decimal Total {
+//        get {
+//            return Quantity * Price;
+//        }
+//    }
+
+//    public InvoiceRow() {
+//        Quantity = 1;
+//    }
+//}
---
+++ OrganizationManagementApp/OrganizationManagementApp/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Starcounter;
+
+namespace OrganizationManagementApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Handle.GET("/invoicedemo", () => {
+                MasterPage master;
+
+                if (Session.Current != null && Session.Current.Data != null)
+                {
+                    master = (MasterPage)Session.Current.Data;
+                }
+                else
+                {
+                    master = new MasterPage();
+
+                    if (Session.Current != null)
+                    {
+                        master.Html = "/InvoiceDemo/LauncherWrapperPage.html";
+                        master.Session = Session.Current;
+                    }
+                    else
+                    {
+                        master.Html = "/InvoiceDemo/MasterPage.html";
+                        master.Session = new Session(SessionOptions.PatchVersioning);
+                    }
+
+                    master.RecentInvoices = new InvoicesPage()
+                    {
+                        Html = "/InvoiceDemo/InvoicesPage.html"
+                    };
+                }
+
+            ((InvoicesPage)master.RecentInvoices).RefreshData();
+                master.FocusedInvoice = null;
+
+                return master;
+            });
+
+            //Handle.GET("/invoicedemo/invoices/{?}", (int InvoiceNo) => {
+            //    MasterPage master = Self.GET<MasterPage>("/invoicedemo");
+            //    master.FocusedInvoice = Db.Scope<InvoicePage>(() => {
+            //        var page = new InvoicePage()
+            //        {
+            //            Html = "/InvoiceDemo/InvoicePage.html",
+            //            Data = Db.SQL<Invoice>("SELECT i FROM Invoice i WHERE InvoiceNo = ?", InvoiceNo).First
+            //        };
+
+            //        page.Saved += (s, a) => {
+            //            ((InvoicesPage)master.RecentInvoices).RefreshData();
+            //        };
+
+            //        page.Deleted += (s, a) => {
+            //            ((InvoicesPage)master.RecentInvoices).RefreshData();
+            //        };
+
+            //        return page;
+            //    });
+            //    return master;
+            //});
+
+            //Handle.GET("/invoicedemo/new-invoice", () => {
+            //    MasterPage master = Self.GET<MasterPage>("/invoicedemo");
+            //    master.FocusedInvoice = Db.Scope<InvoicePage>(() => {
+            //        var page = new InvoicePage()
+            //        {
+            //            Html = "/InvoiceDemo/InvoicePage.html",
+            //            Data = new Invoice()
+            //        };
+
+            //        page.Saved += (s, a) => {
+            //            ((InvoicesPage)master.RecentInvoices).RefreshData();
+            //        };
+
+            //        page.Deleted += (s, a) => {
+            //            ((InvoicesPage)master.RecentInvoices).RefreshData();
+            //        };
+
+            //        return page;
+            //    });
+            //    return master;
+            //});
+
+            Handle.GET("/invoicedemo/menu", () => {
+                MasterPage master = Self.GET<MasterPage>("/invoicedemo");
+                master.ShowOwnTopBarMenu = false;
+                return new Page() { Html = "/InvoiceDemo/AppMenuPage.html" };
+            });
+
+            UriMapping.Map("/invoicedemo/menu", UriMapping.MappingUriPrefix + "/menu");
+            UriMapping.Map("/invoicedemo/app-name", UriMapping.MappingUriPrefix + "/app-name");
+        }
+    }
+}
